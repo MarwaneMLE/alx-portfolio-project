22fc9bcf9ba3a4cfb2b472918e079a9eff3a105b
Marwane <khmarwane10@gmail.com>
2024-09-06 00:36:23 +0200

    version-2

--- application/__init__.py
+++ application/__init__.py
@@ -1,6 +1,22 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 
+app.app_context()
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///expenseDB.db'
+app.config['SECRET_KEY'] = "JLKJJJO3IURYoiogfjjuolnohth@€[jojouuoo=5y9y9youjuy952oohhbafdnoglhoho"
+
+
+db = SQLAlchemy(app)
+app.app_context().push()
+#with app.app_context():
+# # db.create_all()
+
 from application import routes
+ 
+
+# from application import db
+# from application.models import RetailSales
--- run.py
+++ run.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from application import app
+#from application import create_app
 
 #app = Flask(__name__)
- 
+
+#app = create_app()
+
 if __name__ == '__main__':
-    app.run(debug=True, port=3000)
+    app.run(debug=True)
